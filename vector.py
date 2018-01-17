f7965fe0b32d19cc6b4ece57af398f65c4d621a8
Dennis <ludennis0606@gmail.com>
2018-01-16 23:45:32 -0800

    completed quiz 1 and implemented __add__ __sub__ and __mul__

--- vector.py
+++ vector.py
@@ -19,3 +19,26 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, other):
+        return [v1+v2 for v1,v2 in zip(self.coordinates,other.coordinates)]
+
+    def __sub__(self, other):
+        return [v1-v2 for v1,v2 in zip(self.coordinates,other.coordinates)]
+
+    def __mul__(self, other):
+        return [other * v for v in self.coordinates]
+
+vector1 = Vector([8.218,-9.341])
+vector2 = Vector([-1.129,2.111])
+
+print (vector1 + vector2)
+
+vector3 = Vector([7.119,8.215])
+vector4 = Vector([-8.223,0.878])
+
+print (vector3 - vector4) 
+
+vector5 = Vector([1.671,-1.012,-0.318])
+
+print vector5 * 7.41
