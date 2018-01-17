e3d8fda100bb4365d67cedabc4487dc8e5d6f2d2
Dennis <ludennis0606@gmail.com>
2018-01-17 09:30:17 -0800

    added function normalize(self)

--- vector.py
+++ vector.py
@@ -34,8 +34,18 @@ class Vector(object):
             sum += x*x
         return math.sqrt(sum)
 
+    def normalize(self):
+        mag = self.magnitude()
+        return [v / mag for v in self.coordinates]
+
 vector1 = Vector([-0.221,7.437])
 print vector1.magnitude()
 
 vector2 = Vector([8.813,-1.331,-6.247])
 print vector2.magnitude()
+
+vector3 = Vector([5.581,-2.136])
+print vector3.normalize()
+
+vector4 = Vector([1.996,3.108,-4.554])
+print vector4.normalize()
