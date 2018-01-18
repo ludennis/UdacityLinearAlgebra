6a19f412d2d570a357b1ec0d2b811d1c47bd30f4
Dennis <ludennis0606@gmail.com>
2018-01-18 10:13:12 -0800

    added function intercept

--- line.py
+++ line.py
@@ -18,7 +18,7 @@ class Line(object):
         self.normal_vector = normal_vector
 
         if not constant_term:
-            constant_term = Decimal('0')
+            constant_term = Decimal('0.000')
         self.constant_term = Decimal(constant_term)
 
         self.set_basepoint()
@@ -33,7 +33,7 @@ class Line(object):
             initial_index = Line.first_nonzero_index(n)
             initial_coefficient = n[initial_index]
 
-            basepoint_coords[initial_index] = c/initial_coefficient
+            basepoint_coords[initial_index] = c/Decimal(initial_coefficient)
             self.basepoint = Vector(basepoint_coords)
 
         except Exception as e:
@@ -89,9 +89,36 @@ class Line(object):
         return output
 
 
+    def intercept(self,other):
+        # |A|   |c|
+        # |B| x |y| = k1 
+        #   
+        # |C|   |x|
+        # |D| x |y| = k2
+        #
+        # x = (Dk1 - Bk2)/(AD-BC)
+        # y = (-Ck1 + Ak2)/(AD-BC)
+        self_norm,targ_norm=self.normal_vector,other.normal_vector
+
+        A,B,k1 = self_norm[0],self_norm[1],self.constant_term
+        C,D,k2 = targ_norm[0],targ_norm[1],other.constant_term
+
+        # check if two normal vectors are parallel
+        if not self_norm.is_parallel(targ_norm):
+            #not parallel, need to find intercept
+            x = (D * k1 - B * k2) / (A * D - B * C)
+            y = (-C * k1 + A * k2) / (A * D - B * C)
+            return tuple([x,y])
+        else:
+            return self.is_incident(other)
+
+
+    def is_incident(self,other):
+        return False
+
     @staticmethod
     def first_nonzero_index(iterable):
-        for k, item in enumerate(iterable):
+        for k, item in enumerate(iterable.coordinates):
             if not MyDecimal(item).is_near_zero():
                 return k
         raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
@@ -100,3 +127,18 @@ class Line(object):
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
+
+
+line1 = Line(Vector([4.046,2.836]),1.21)
+line2 = Line(Vector([10.115,7.09]),3.025)
+
+print (line1.intercept(line2))
+
+line1 = Line(Vector([7.204,3.182]),8.68)
+lien2 = Line(Vector([8.172,4.114]),9.883)
+
+print (line1.intercept(line2))
+
+
+line1 = Line(Vector([1.182,5.562]),6.744)
+line2 = Line(Vector([1.773,8.343]),9.525)
