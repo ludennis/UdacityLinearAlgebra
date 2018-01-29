c74f2492a12209b708e988c07d20bd219ca88f4b
Dennis <ludennis0606@gmail.com>
2018-01-29 14:32:58 -0800

    commented out test cases

--- plane.py
+++ plane.py
@@ -110,22 +110,22 @@ class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
         return abs(self) < eps
 
-plane1 = Plane(Vector([-0.412,3.806,0.728]),-3.46)
-plane2 = Plane(Vector([1.03,-9.515,-1.82]),8.65)
+# plane1 = Plane(Vector([-0.412,3.806,0.728]),-3.46)
+# plane2 = Plane(Vector([1.03,-9.515,-1.82]),8.65)
 
-print(plane1==plane2)
-print(plane1.is_parallel(plane2))
-
-
-plane3 = Plane(Vector([2.611,5.528,0.283]),4.6)
-plane4 = Plane(Vector([7.715,8.306,5.342]),3.76)
-
-print(plane3==plane4)
-print(plane3.is_parallel(plane4))
+# print(plane1==plane2)
+# print(plane1.is_parallel(plane2))
 
 
-plane5 = Plane(Vector([-7.926,8.625,-7.217]),-7.952)
-plane6 = Plane(Vector([-2.642,2.875,-2.404]),-2.443)
+# plane3 = Plane(Vector([2.611,5.528,0.283]),4.6)
+# plane4 = Plane(Vector([7.715,8.306,5.342]),3.76)
 
-print(plane5==plane6)
-print(plane5.is_parallel(plane6))
+# print(plane3==plane4)
+# print(plane3.is_parallel(plane4))
+
+
+# plane5 = Plane(Vector([-7.926,8.625,-7.217]),-7.952)
+# plane6 = Plane(Vector([-2.642,2.875,-2.404]),-2.443)
+
+# print(plane5==plane6)
+# print(plane5.is_parallel(plane6))
